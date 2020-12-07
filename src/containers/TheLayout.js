a4a10f9fe6b0390d164690c3bb5d38b0804ca1e1
irwanphan <irwanphan@gmail.com>
2020-12-07 14:42:22 +0700

    button drop shadow using sass function, bugs, shaded from last color in list

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -2,7 +2,10 @@ const TheLayout = () => {
     return (
         <header>
             header
+            <span className="button green">asdf</span>
             <span className="button ok">asdf</span>
+            <span className="button blue">asdf</span>
+            <span className="button pink">asdf</span>
         </header>
     )
 }
